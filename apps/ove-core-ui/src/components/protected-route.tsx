d85980b52363c42b87bdcce28bd0bec071c75920
bcd00 <bcd004@outlook.com>
2023-05-23 14:10:39 +0100

    Implementing authentication on the core

---
+++ apps/ove-core-ui/src/components/protected-route.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+import { Navigate } from 'react-router-dom'
+
+/**
+ * Only allows navigation to a route if a condition is met.
+ * Otherwise, it redirects to a different specified route.
+ */
+export default ({
+  condition,
+  redirectTo,
+  children,
+}: ConditionalRouteProps): JSX.Element => condition ? <>{children}</> :
+  <Navigate to={redirectTo} replace />
+
+export type ConditionalRouteProps = {
+  /**
+   * Route is created if its condition is true.
+   * For example, `condition={isLoggedIn}` or `condition={isAdmin}`
+   */
+  condition: boolean
+
+  /** The route to redirect to if `condition` is false */
+  redirectTo: string
+
+  children?: ReactNode
+}
