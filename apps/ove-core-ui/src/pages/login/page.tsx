47938508327533d792d2c5a93e85c4d1ea083b6a
bcd00 <bcd004@outlook.com>
2023-07-05 10:05:14 +0100

    Updating login UI

--- apps/ove-core-ui/src/pages/login/page.tsx
+++ apps/ove-core-ui/src/pages/login/page.tsx
@@ -38,17 +38,16 @@ export default ({ setTokens }: PageProps) => {
     navigate("/", { replace: true });
   }, []);
 
-  return <main>
-    <h1>Login</h1>
+  return <main style={{display: "flex", flexDirection: "column", width: "100vw", justifyContent: "center", alignItems: "center"}}>
     <form method="post" spellCheck="false" onSubmit={e => {
       handleAuth(e).catch(console.error);
-    }}>
-      <h4>Enter credentials:</h4>
-      <label htmlFor="username">Enter Username:</label>
-      <input id="username" type="text" name="username" />
-      <label htmlFor="password">Enter Password:</label>
-      <input id="password" type="password" name="password" />
-      <button type="submit">Log In</button>
+    }} style={{display: "flex", flexDirection: "column", width: "20vw", marginTop: "2rem"}}>
+      <h1 style={{fontSize: "24px", fontWeight: "700"}}>Sign in</h1>
+      <label htmlFor="username" style={{fontWeight: "700", marginTop: "2rem"}}>Username</label>
+      <input id="username" type="text" name="username" style={{border: "1px solid black", padding: "0.5rem", borderRadius: "20px"}} />
+      <label htmlFor="password" style={{fontWeight: "700", marginTop: "1rem"}}>Password</label>
+      <input id="password" type="password" name="password" style={{border: "1px solid black", padding: "0.5rem", borderRadius: "20px"}} />
+      <button type="submit" style={{color: "white", backgroundColor: "#002147", marginTop: "3rem", padding: "0.5rem", borderRadius: "20px"}}>Sign In</button>
     </form>
   </main>;
 }
