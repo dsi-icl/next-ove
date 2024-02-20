c14390c25964b0f0a3ccac6d2714ed6b936830be
bcd00 <bcd004@outlook.com>
2024-02-20 19:13:02 +0000

    Dockerizing core services and adding package deprecation analysis

---
+++ aqua-nautilus/analyze.sh
@@ -0,0 +1,7 @@
+cd aqua-nautilus || exit
+cp ../package.json ./
+docker build -t aqua-nautilus:latest .
+rm -rf package.json
+img_id=$(docker create aqua-nautilus:latest)
+docker cp "$img_id":/usr/src/app/Dependency-Deprecated-Checker/analysis.txt ./
+docker rm -v "$img_id"
