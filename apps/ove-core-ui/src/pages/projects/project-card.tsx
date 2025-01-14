11e3b046944b4b9e1317424c3a1973c7d0ad3b7d
Brython Caley-Davies <bcd004@outlook.com>
2025-01-14 17:57:43 +0000

    Adding description to project card and removing CSS module

--- apps/ove-core-ui/src/pages/projects/project-card.tsx
+++ apps/ove-core-ui/src/pages/projects/project-card.tsx
@@ -3,13 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@ove/ui-base-components";
 import type { Project, User } from "@prisma/client";
 
-import styles from "./project-card.module.scss";
-
 type ProjectCardProps = {
   user: User
   project: Project
   openDialog: () => void
 }
+
+const limitText = (text: string, limit: number) => text.length > limit ? `${text.slice(0, limit)}…` : text;
 
 const ProjectCard = ({
   user,
@@ -21,18 +21,19 @@ const ProjectCard = ({
     ((user.id === project.creatorId ||
       project.collaboratorIds.includes(user.id)) && user.role !== "client");
 
-  return <li key={project.title} className={styles.main}>
-    <img src={project.thumbnail ?? "/missing-thumbnail.jpg"}
+  return <li key={project.title} className="p-3 max-w-[calc(1.5rem + 256px)] rounded-xl border-[1px] border-gray-200">
+    <img className="w-full aspect-square rounded-xl" src={project.thumbnail ?? "/missing-thumbnail.jpg"}
          alt={`Thumbnail for ${project.title}`} />
-    <h4>{project.title}</h4>
-    <div className={styles.actions}>
+    <h4 className="text-black mt-2 font-bold">{project.title}</h4>
+    <p className="text-black mt-1">{limitText(project.description, 140)}</p>
+    <div className="flex justify-between mt-2">
       {canEdit ? <Button
-        className="rounded-r-none"
+        className="rounded-r-none w-full"
         variant="outline"
         onClick={() => navigate(`/project-editor?project=${project.id}`)}>
         EDIT
       </Button> : null}
-      <Button onClick={openDialog} className="rounded-l-none">
+      <Button onClick={openDialog} className="rounded-l-none w-full">
         LAUNCH
       </Button>
     </div>
