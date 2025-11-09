588b18ee964a647105b511d17b473ef217cbafc9
Brython Caley-Davies <bcd004@outlook.com>
2025-11-09 01:44:55 +0000

    Fixing calendar integration to use ical link

--- apps/ove-core-ui/src/pages/hardware/components/toolbar/calendar.tsx
+++ apps/ove-core-ui/src/pages/hardware/components/toolbar/calendar.tsx
@@ -26,18 +26,18 @@ const Calendar = ({ bridgeId }: { bridgeId: string }) => {
         </Button>
       </DialogTrigger>
       <DialogContent
-        className="w-[65vw] max-w-[unset]"
+        className="w-[90vw] h-[90vh] max-w-[unset] flex flex-col"
         aria-description="calendar content"
       >
-        <DialogHeader className="h-fit">
+        <DialogHeader>
           <DialogTitle>Observatory Calendar</DialogTitle>
           <DialogDescription>View usage of the observatory</DialogDescription>
         </DialogHeader>
-        <div className="flex h-[40vh] w-full flex-col items-center">
+        <div className="flex h-[calc(90vh-16rem)] mt-8 w-full flex-col items-center">
           <CalendarView calendar={calendar ?? []} />
-          <div className="flex w-full py-4">
-            <h4>Last Updated:</h4>
-            <p className="ml-auto">{lastUpdated}</p>
+          <div className="flex w-full py-4 mt-auto">
+            <h4 className="font-bold">Last Updated:</h4>
+            <p className="ml-auto font-medium">{lastUpdated}</p>
           </div>
         </div>
       </DialogContent>
